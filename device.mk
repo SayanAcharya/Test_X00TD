8b55e1734ef21d4859401c37880ab199c4644fc6
AryanPatidar <ap9977925180@gmail.com>
2018-10-09 14:54:20 +0000

    X00TD: Tweaks For Ram Management
    Sunny's Ram Management Tweaks...

--- device.mk
+++ device.mk
@@ -449,6 +449,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Tweak's Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true
+    ro.sys.fw.bg_apps_limit=32
+    ro.config.dha_cached_max=16
+    ro.config.dha_empty_max=42
+    ro.config.dha_empty_init=32
+    ro.config.dha_lmk_scale=0.545
+    ro.config.dha_th_rate=2.3
+    ro.config.sdha_apps_bg_max=64
+    ro.config.sdha_apps_bg_min=8
+
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service.basic
